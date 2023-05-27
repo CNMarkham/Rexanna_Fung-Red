1e814b6f562f1d224ff12c0ad781e3df3bbea958
Israel1129 <Israel.semple@gmail.com>
2023-05-27 12:00:26 -0400

    step 73

---
+++ RHF-Gravity Trails/Assets/Scripts/Throwable.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Throwable : MonoBehaviour
+{
+
+    public GameObject objectThrown;
+
+    public Vector3 offset;
+
+    public int throwablecounter;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1") && throwablecounter > 0)
+        {
+            Vector3 throwablePosition = transform.position + offset;
+            offset = new Vector3(1, 0, 0);
+            offset = transform.localScale.x * new Vector3(1, 0, 0);
+            Instantiate(objectThrown, throwablePosition, transform.rotation);
+            throwablecounter -= 1;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Collectable")
+        {
+            throwablecounter += 1;
+            Destroy(collision.gameObject);
+        }
+    }
+
+}
