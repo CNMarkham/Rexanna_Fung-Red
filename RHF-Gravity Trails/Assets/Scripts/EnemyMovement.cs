cf161132a3648dc9e764d9df350932dfd5e8a1f1
Ryan <ryanjunmingli@gmail.com>
2023-07-08 11:51:31 -0400

    step 116

--- RHF-Gravity Trails/Assets/Scripts/EnemyMovement.cs
+++ RHF-Gravity Trails/Assets/Scripts/EnemyMovement.cs
@@ -16,6 +16,8 @@ public class EnemyMovement : MonoBehaviour
 
     public int minimumPosition;
 
+    public Teleport teleport;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -38,8 +40,10 @@ public class EnemyMovement : MonoBehaviour
 
         if (collision.gameObject.tag == "ThrowingObject")
         {
+            teleport.enemyCount -= 1;
             Destroy(collision.gameObject);
             Destroy(this.gameObject);
+
         }
     }
 
@@ -63,5 +67,8 @@ public class EnemyMovement : MonoBehaviour
     {
        return true;
     }
+
+
+
 
 }
