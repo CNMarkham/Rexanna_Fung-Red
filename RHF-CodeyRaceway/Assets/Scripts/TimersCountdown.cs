343d7090d41556e422da7ba6c0b2c26e72516c80
RyanLi4 <123781912+MariePlush@users.noreply.github.com>
2023-09-16 11:59:12 -0400

    115

--- RHF-CodeyRaceway/Assets/Scripts/TimersCountdown.cs
+++ RHF-CodeyRaceway/Assets/Scripts/TimersCountdown.cs
@@ -6,6 +6,7 @@ using UnityEngine.UI;
 public class TimersCountdown : MonoBehaviour
 {
 
+    public CodeyMove codey; 
     public Text lapTime;
     public Text startCountdown;
 
@@ -17,9 +18,25 @@ public class TimersCountdown : MonoBehaviour
     {
         totalLapTime -= Time.deltaTime;
 
-        totalCountdownTime -= Time.deltaTime;
 
         lapTime.text = Mathf.Round(totalLapTime).ToString();
         startCountdown.text = Mathf.Round(totalCountdownTime).ToString();
+        if (totalCountdownTime > 0f)
+        {
+            totalCountdownTime -= Time.deltaTime;
+            startCountdown.text = totalCountdownTime.ToString();
+            codey.Speed = 0f;
+        }
+        if (totalCountdownTime <= 0f)
+        {
+            startCountdown.text = "";
+            totalLapTime -= Time.deltaTime;
+            lapTime.text = totalLapTime.ToString();
+            codey.Speed = 1500;
+        }
+        if (totalCountdownTime < 0f)
+        {
+            Debug.Log("Time's up!");
+        }
     }
 }
