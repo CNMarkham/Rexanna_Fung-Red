2996d39b6d7cd5d801b7cb20c9bd6b6306b98a1b
Iroan Meen <markhamonca@codeninjas.com>
2023-11-16 19:51:46 -0500

    step 46

--- RHF-SulkySlimes/Assets/Scripts/Collectable.cs
+++ RHF-SulkySlimes/Assets/Scripts/Collectable.cs
@@ -10,16 +10,38 @@ public class Collectable : MonoBehaviour
     private Vector3 startingPosition;
     private Vector3 endingPosition;
 
+    public float speed = 0.1f;
+    public float direction = -1f;
+
     // Start is called before the first frame update
     void Start()
     {
         startingPosition = transform.position;
-
+        endingPosition = startingPosition + new Vector3(distanceToMove, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(transform.position.x < startingPosition.x)
+        {
+            direction = 1f;
+        }
+
+        if(transform.position.x > startingPosition.x)
+        {
+            direction = -1f;
+        }
+
+        transform.position += new Vector3(speed * direction * Time.deltaTime, 0, 0);
     }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
 }
